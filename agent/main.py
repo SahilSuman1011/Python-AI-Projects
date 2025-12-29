42133c70128cf121d6dd7a5787a4abf61fb5c5a6
Sahil Suman <sahilsuman1202@gmail.com>
2025-12-29 18:06:34 +0530

    complete basic agent

--- agent/main.py
+++ agent/main.py
@@ -2,23 +2,46 @@ from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from dotenv import load_dotenv
 from langchain.tools import tool
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
 
 import os
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 # Load environment variables from .env file
 load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
+
+@tool
+def calculator(a: float, b: float) -> str:
+    """Useful for performing basic arithmetic operations."""
+    print("tool has been called.")
+    return f"The Sum of {a} and {b} is {a + b}."
+
 
 # Initialize Gemini LLM
-llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
+def main():
+    model = ChatGoogleGenerativeAI(model="gemini-3-pro-preview")
 
-tools = []
-agent_executor = create_react_agent(model, tools) 
+    tools = [calculator]
+    agent_executor = create_agent(model, tools)
 
-print("Agent is ready to use. Type 'quit' to exit.")
-print("You can ask questions like 'What is the capital of France?' or 'Who won the World Series in 2020?'")
+    print("Agent is ready to use. Type 'quit' to exit.")
+    print("You can ask questions like 'What is the capital of France?' or 'Who won the World Series in 2020?'")
 
-while True:
-    user_input = input("\nYou: ").strip()
+    while True:
+        user_input = input("\nYou: ").strip()
+
+        if user_input == "quit":
+            break
+
+        print("\nAssistant: ", end="")
+        for chunk in agent_executor.stream(
+            {"messages": [HumanMessage(content=user_input)]}
+    ):
+        
+            if "agent" in chunk and "messages" in chunk["agent"]:
+                for message in chunk["agent"]["messages"]:
+                    print(message.content, end="")
+    print()
+
+if __name__ == "__main__":
+    main()
